b88eb6729aae847f4756bd8beae4aad9fa86085d
Helen Tsui <helentsui.tht@gmail.com>
2023-02-03 22:07:37 +0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `yahoo-finance-site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
